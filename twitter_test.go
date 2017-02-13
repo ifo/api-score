705b8d8d95607bab9e4f4ab3fe643973ff2f9889
Steve McCarthy <steve@redlua.com>
2017-02-13 14:01:48 -0500

    Finalize tests

--- twitter_test.go
+++ twitter_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"testing"
 	"time"
 
@@ -8,15 +9,53 @@ import (
 )
 
 func Test_tweetRateAdjustment(t *testing.T) {
-	t.Skip()
+	limit := time.Now().AddDate(0, 0, -6)
+	newTweet := makeTweet(0, limit.AddDate(0, 0, 5))
+	oldTweet := makeTweet(0, limit.AddDate(0, 0, 1))
+
+	cases := map[int]struct {
+		In         []anaconda.Tweet
+		DayLimit   int
+		Adjustment string
+		Err        error
+	}{
+		1: {In: []anaconda.Tweet{newTweet, newTweet, oldTweet, oldTweet, oldTweet, oldTweet}, DayLimit: 6, Adjustment: "0.15", Err: nil},
+	}
+
+	for num, c := range cases {
+		adj, err := tweetRateAdjustment(c.In, c.DayLimit)
+		if fmt.Sprintf("%0.2f", adj) != c.Adjustment {
+			t.Errorf("Actual: %0.2f, Expected: %0.2f, Case: %d", adj, c.Adjustment, num)
+		}
+		if err != c.Err {
+			t.Errorf("Actual: %+v, Expected: %+v, Case: %d", err, c.Err, num)
+		}
+	}
 }
 
 func Test_tweetReplyRatioAdjustment(t *testing.T) {
-	t.Skip()
+	replyTweet := makeTweet(2, time.Now())
+	normalTweet := makeTweet(0, time.Now())
+
+	cases := map[int]struct {
+		In         []anaconda.Tweet
+		Adjustment float64
+	}{
+		1: {In: []anaconda.Tweet{replyTweet, replyTweet, normalTweet}, Adjustment: 0.0},
+		2: {In: []anaconda.Tweet{replyTweet}, Adjustment: 0.3},
+		3: {In: []anaconda.Tweet{normalTweet}, Adjustment: 0.5},
+	}
+
+	for num, c := range cases {
+		adj := tweetReplyRatioAdjustment(c.In, 1)
+		if adj != c.Adjustment {
+			t.Errorf("Actual: %0.1f, Expected: %0.1f, Case: %d", adj, c.Adjustment, num)
+		}
+	}
 }
 
 func Test_getTweets(t *testing.T) {
-	t.Skip("TODO: Modify anaconda.TwitterApi to send to getTweets")
+	t.Skip("TODO: Modify anaconda.TwitterApi.HttpClient to be a test client")
 }
 
 func Test_filterOlderTweets(t *testing.T) {
@@ -50,6 +89,7 @@ func Test_filterOlderTweets(t *testing.T) {
 func makeTweet(replyId int64, date time.Time) anaconda.Tweet {
 	return anaconda.Tweet{
 		InReplyToUserID: replyId,
+		User:            anaconda.User{Id: 1},
 		CreatedAt:       date.Format(time.RubyDate),
 	}
 }
